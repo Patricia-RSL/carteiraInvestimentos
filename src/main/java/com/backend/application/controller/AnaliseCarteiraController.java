8a40e923202ddc8114e6f667ca51a1a3e77f0b01
Patricia Lima <lima.patriciars@gmail.com>
2025-03-21 17:54:37 -0300

    feat: adição de documentação com swagger

--- src/main/java/com/backend/application/controller/AnaliseCarteiraController.java
+++ src/main/java/com/backend/application/controller/AnaliseCarteiraController.java
@@ -1,8 +1,12 @@
 package com.backend.application.controller;
 
 import com.backend.application.dto.AnaliseCarteiraResponseDTO;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,21 +20,26 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 @Log4j2
 @RequestMapping("/api/analise-carteira")
+@Tag(name = "AnaliseCarteiraController", description = "Endpoint para obter analises da carteira de ativos")
 public class AnaliseCarteiraController {
 
     private final AnaliseCarteiraService analiseCarteiraService;
-    
-    @PostMapping("/analisar")
-    public ResponseEntity<AnaliseCarteiraResponseDTO> analisarCarteira(@RequestBody AnaliseCarteiraRequestDTO request) {
-        return ResponseEntity.ok().body(analiseCarteiraService.analiseCarteira(request));
 
+    @Operation(summary = "Analisar carteira de ativos", description = "Retorna análises da carteira de ativos")
+    @ApiResponse( responseCode = "200", description = "Resultado das análises da carteira obtido com sucesso",
+            content = { @Content(mediaType = "application/json",
+            schema = @Schema(implementation = AnaliseCarteiraResponseDTO.class)) })
     @GetMapping("/analisar")
     public ResponseEntity<AnaliseCarteiraResponseDTO> analisarCarteira() {
         return ResponseEntity.ok().body(analiseCarteiraService.analiseCarteira());
     }
 
+    @Operation(summary = "Analisar carteira de ativos com filtro", description = "Retorna análises da carteira de ativos de acordo com filtros fornecidos")
+    @ApiResponse( responseCode = "200", description = "Resultado das análises da carteira obtido com sucesso",
+            content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = AnaliseCarteiraResponseDTO.class)) })
     @PostMapping("/analisar")
-    public ResponseEntity<AnaliseCarteiraResponseDTO> analisarCarteiraComFiltro(@RequestBody AnaliseCarteiraRequestDTO request) {
+    public ResponseEntity<AnaliseCarteiraResponseDTO> analisarCarteiraComFiltro(@Parameter(description = "filtros") @RequestBody AnaliseCarteiraRequestDTO request) {
         return ResponseEntity.ok().body(analiseCarteiraService.analiseCarteiraComFiltro(request.init()));
     }
 }
--- src/main/java/com/backend/application/controller/UserTradeController.java
+++ src/main/java/com/backend/application/controller/UserTradeController.java
@@ -3,6 +3,12 @@ package com.backend.application.controller;
 import java.util.List;
 import java.util.Optional;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,18 +25,27 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 @Log4j2
 @RequestMapping("/api/usertrade")
+@Tag(name = "UserTradeController", description = "Endpoint para obter transações de usuários")
 public class UserTradeController {
 
     private final UserTradeService userTradeService;
 
-
+    @Operation(summary = "Obter todas as transações", description = "Retorna uma lista de todas as transações de usuários")
+    @ApiResponse(responseCode = "200", description = "Lista de transações obtida com sucesso",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserTrade.class))})
     @GetMapping("/")
     public ResponseEntity<List<UserTrade>> getAll() {
         return ResponseEntity.ok().body(userTradeService.getAll());
     }
 
+    @Operation(summary = "Obter transação por ID", description = "Retorna uma transação específica pelo ID")
+    @ApiResponse(responseCode = "200", description = "Transação obtida com sucesso",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserTrade.class))})
+    @ApiResponse(responseCode = "404", description = "Transação não encontrada")
     @GetMapping("/{id}")
-    public ResponseEntity<UserTrade> getById(@PathVariable Long id) {
+    public ResponseEntity<UserTrade> getById(@Parameter(description = "ID da transação") @PathVariable Long id) {
         Optional<UserTrade> optTrade = userTradeService.getById(id);
 
         if(optTrade.isPresent()){
@@ -38,19 +53,23 @@ public class UserTradeController {
         }
 
         return ResponseEntity.notFound().build();
-        
     }
 
+    @Operation(summary = "Obter transações por ativo", description = "Retorna todas as transações associadas a um ativo específico")
+    @ApiResponse(responseCode = "200", description = "Lista de transações obtida com sucesso",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserTrade.class))})
     @GetMapping("/instrument/{instrument}")
-    public ResponseEntity<List<UserTrade>> getByInstrument(@PathVariable String instrument) {
+    public ResponseEntity<List<UserTrade>> getByInstrument(@Parameter(description = "Nome do instrumento") @PathVariable String instrument) {
         return ResponseEntity.ok().body(userTradeService.findAllByInstrument(instrument));
-        
     }
 
+    @Operation(summary = "Obter transações por tipo de operação", description = "Retorna todas as transações associadas a um tipo de operação específico")
+    @ApiResponse(responseCode = "200", description = "Lista de transações obtida com sucesso",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserTrade.class))})
     @GetMapping("/tipo-operacao/{tipoOperacao}")
-    public ResponseEntity<List<UserTrade>> getByTipoOpeacao(@PathVariable TipoOperacao tipoOperacao) {
+    public ResponseEntity<List<UserTrade>> getByTipoOpeacao(@Parameter(description = "Tipo de operação") @PathVariable TipoOperacao tipoOperacao) {
         return ResponseEntity.ok().body(userTradeService.findAllByTipoOperacao(tipoOperacao));
-        
     }
-    
 }

4d68fd3a8b33716bacc5a87fd74f49451e0ddcdf
Patricia Lima <lima.patriciars@gmail.com>
2025-03-24 19:08:43 -0300

    feat: override de equals e hashCode

--- src/main/java/com/backend/application/dto/ItemDetalhesAnaliseCarteiraDTO.java
+++ src/main/java/com/backend/application/dto/ItemDetalhesAnaliseCarteiraDTO.java
@@ -1,6 +1,7 @@
 package com.backend.application.dto;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 import lombok.Data;
 
@@ -12,4 +13,30 @@ public class ItemDetalhesAnaliseCarteiraDTO {
     private BigDecimal valorInvestido;
     private BigDecimal valorMercado;
     private BigDecimal rendimentosPorcentagem;
+
+    public ItemDetalhesAnaliseCarteiraDTO init(String instrument, int totalAcoes, BigDecimal valorInvestido, BigDecimal valorMercado, BigDecimal rendimentosPorcentagem) {
+        this.instrument = instrument;
+        this.valorInvestido = valorInvestido;
+        this.valorMercado = valorMercado;
+        this.qtdAcoes = totalAcoes;
+        this.rendimentosPorcentagem = rendimentosPorcentagem;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemDetalhesAnaliseCarteiraDTO argument = (ItemDetalhesAnaliseCarteiraDTO) o;
+        return argument.getInstrument().equals(this.getInstrument()) &&
+                argument.getQtdAcoes() == this.getQtdAcoes() &&
+                argument.getValorInvestido().compareTo(this.getValorInvestido()) == 0 &&
+                (argument.getValorMercado() == null ? this.getValorMercado() == null : argument.getValorMercado().compareTo(this.getValorMercado()) == 0) &&
+                (argument.getRendimentosPorcentagem() == null ? this.getRendimentosPorcentagem() == null : argument.getRendimentosPorcentagem().compareTo(this.getRendimentosPorcentagem()) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(instrument, qtdAcoes, valorInvestido, valorMercado, rendimentosPorcentagem);
+    }
 }
